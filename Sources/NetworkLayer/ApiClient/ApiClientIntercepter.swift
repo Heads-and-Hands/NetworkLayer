78f8be5dcc46fbc0dc2c4cdd316f8215aaab7061
Vlad Zaicev <v.zaicev@headsandhands.ru>
2021-04-09 15:33:55 +0300

    Fix retry

--- Sources/NetworkLayer/ApiClient/ApiClientIntercepter.swift
+++ Sources/NetworkLayer/ApiClient/ApiClientIntercepter.swift
@@ -127,6 +127,9 @@ public class ApiClientExpiredTokenIntercepter: RequestInterceptor, ApiClientFini
 
     private func drainContainers(with retryResult: RetryResult) {
         containers.forEach { $0.completion(retryResult) }
-        containers.removeAll()
+
+        queue.async { [weak self] in
+            self?.containers.removeAll()
+        }
     }
 }
